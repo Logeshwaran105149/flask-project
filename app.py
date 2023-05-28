d34d6a152b205c1ef2ce0dbe8865879cd50fc8fb
Logeshwaran105149 <logeshwaran.velmurugan@my.jcu.edu.au>
2023-05-28 22:09:28 +0800

    flask project

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<h1> Hello world! </h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    if name:
+        return f"Hello, {name}!"
+    else:
+        return "Hello!"
+
+
+def cel_to_fahrenheit(cel):
+    return cel * 9.0 / 5.0 + 32
+
+
+@app.route('/convert/<celsius>')
+def convert(celsius):
+    try:
+        fahrenheit = cel_to_fahrenheit(float(celsius))
+        return f"The temperature in Fahrenheit is {fahrenheit}"
+    except ValueError:
+        abort(400, description="Invalid temperature provided.")
+
+
+app.run(debug=True)
